dfb8a81fc3993a8aae7a09e29c13dc5cf48d71b1
Azamat <55547016+OpTi9@users.noreply.github.com>
2023-04-24 22:15:08 +0200

    implementing google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,65 +1,74 @@
-import {NextAuthOptions} from "next-auth";
-import {UpstashRedisAdapter} from "@next-auth/upstash-redis-adapter";
-import {db} from "@/lib/db";
-import GoogleProvider from "next-auth/providers/google";
-import {redirect} from "next/navigation";
+import { NextAuthOptions } from 'next-auth'
+import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter'
+import { db } from './db'
+import GoogleProvider from 'next-auth/providers/google'
+import { fetchRedis } from '@/helpers/redis'
 
 function getGoogleCredentials() {
-    const clientId = process.env.GOOGLE_CLIENT_ID;
-    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
     if (!clientId || clientId.length === 0) {
-        throw new Error("GOOGLE_CLIENT_ID is not set");
-    }
-    if (!clientSecret || clientSecret.length === 0) {
-        throw new Error("GOOGLE_CLIENT_SECRET is not set");
+        throw new Error('Missing GOOGLE_CLIENT_ID')
     }
 
-    return {clientId, clientSecret};
+    if (!clientSecret || clientSecret.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_SECRET')
+    }
+
+    return { clientId, clientSecret }
 }
 
-// we use jwt (json web token) to verify session in our middleware
-export const authOptions : NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
     adapter: UpstashRedisAdapter(db),
     session: {
-        strategy: "jwt",
+        strategy: 'jwt',
     },
+
     pages: {
-        signIn: "/login",
+        signIn: '/login',
     },
     providers: [
         GoogleProvider({
             clientId: getGoogleCredentials().clientId,
             clientSecret: getGoogleCredentials().clientSecret,
-        })
+        }),
     ],
     callbacks: {
-        async jwt({token, user}) {
-            // check if user is already in db
-            const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+        async jwt({ token, user }) {
+            const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as
+                | string
+                | null
 
-            if(!dbUser) {
-                token.id = user!.id;
-                return token;
+            if (!dbUserResult) {
+                if (user) {
+                    token.id = user!.id
+                }
+
+                return token
             }
+
+            const dbUser = JSON.parse(dbUserResult) as User
 
             return {
                 id: dbUser.id,
                 name: dbUser.name,
                 email: dbUser.email,
-                image: dbUser.image,
+                picture: dbUser.image,
             }
         },
-        async session({session, token}) {
+        async session({ session, token }) {
             if (token) {
-                session.user.id = token.id;
-                session.user.name = token.name;
-                session.user.email = token.email;
-                session.user.image = token.image as string;
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
             }
-            return session;
+
+            return session
         },
         redirect() {
-            return redirect("/dashboard");
-        }
-    }
+            return '/dashboard'
+        },
+    },
 }
