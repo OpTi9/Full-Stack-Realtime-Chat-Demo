db9174b1d3821f67651a30342177c58065afdc22
Azamat <55547016+OpTi9@users.noreply.github.com>
2023-04-30 17:44:01 +0200

    - bugfix: fixed add friend counter updating

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -19,16 +19,23 @@ const FriendRequestSidebarOptions: FC<FriendRequestsSidebarOptionsProps> = ({ini
     // real time updates
     useEffect(() => {
         pusherClient.subscribe(toPusherKey(`user:${sessionId}:incoming_friend_requests`));
-
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`));
         const friendRequestHandler = () => {
             setUnseenRequestsCount((prev) => prev + 1);
         };
 
+        const addedFriendHandler = () => {
+            setUnseenRequestsCount((prev) => prev - 1);
+        };
+
         pusherClient.bind('incoming_friend_requests', friendRequestHandler);
+        pusherClient.bind('new_friend', addedFriendHandler);
 
         return () => {
             pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:incoming_friend_requests`));
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
             pusherClient.unbind('incoming_friend_requests', friendRequestHandler);
+            pusherClient.unbind('new_friend', addedFriendHandler);
         };
     },[sessionId]);
 
